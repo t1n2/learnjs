fc3e7029d65347d0eb60299084d910302d6e540f
Misha <xz@mail.ru>
2020-09-20 18:27:01 +0300

    Modification (Part 3)

--- home/js/script.js
+++ home/js/script.js
@@ -1,13 +1,18 @@
 'use strict';
 
-/* 1 */ 
+let numberOfFilms;
 
-let numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+function start() {
+	numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
 
+	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+	}
+}
 
-/* 2 */
+start();
 
-let personaMovieDB = {
+const personaMovieDB = {
 	count: numberOfFilms,
 	movies: {},
 	actors: {},
@@ -15,20 +20,40 @@ let personaMovieDB = {
 	privat: false
 }
 
-let checkCount = (personaMovieDB.count < 10) ? alert('Просмотрено довольно мало фильмов') : 
-   (personaMovieDB.count >= 10 && personaMovieDB.count <= 30) ? alert('Вы классический зритель') :
-   (personaMovieDB.count > 30) ? alert('Вы киноман') :
-   alert ('Произошла ошибка');
 
-
-/* 3 */
-
-for (var i = 0; i < 2; i++) {
+function rememberMyFilms () {
+	for (var i = 0; i < 2; i++) {
 	let a = prompt('Один из последних фильмов?', ''),
 	    b = prompt('На сколько оцените его?', '');
 	if ((a || b !== '') && (a || b !== null) && (a.length < 50)) {
 		personaMovieDB.movies[a] = b;
 	} else i--;
+	}
 }
 
-console.log(personaMovieDB.movies)
+rememberMyFilms();
+
+function detectPErsonalLevel() {
+	let checkCount = (personaMovieDB.count < 10) ? alert('Просмотрено довольно мало фильмов') : 
+   (personaMovieDB.count >= 10 && personaMovieDB.count <= 30) ? alert('Вы классический зритель') :
+   (personaMovieDB.count > 30) ? alert('Вы киноман') :
+   alert ('Произошла ошибка');
+}
+
+detectPErsonalLevel();
+
+function writeYourGenres() {
+	for (var i = 0; i < 3; i++) {
+		personaMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i+1}`, '')
+	}
+}
+
+writeYourGenres();
+
+function showDB() {
+	if (!personaMovieDB.privat) {
+		console.log(personaMovieDB)
+	} 
+}
+
+showDB();
---
+++ home/js/test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const str = 'test';
+
+console.log(str.toUpperCase());
+
+let fruit = "Banana mama"
+
+console.log(fruit.indexOf('mama'))
+
+const logg = 'Hello world';
+
+// console.log(logg.slice(6, 11));
+
+// console.log(logg.substring(6, 11));
+
+// console.log(logg.substr(6, 3));
+
+const num = 12.2;
+
+console.log(Math.round(num));
+
+const test ='12.2px';
+
+console.log((parseInt(test));
